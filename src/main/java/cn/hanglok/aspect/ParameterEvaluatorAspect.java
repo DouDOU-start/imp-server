406270e162f89511d42a58242678ea091d772d27
Allen <allen.huang@hanglok-tech.cn>
2023-06-16 17:02:36 +0800

    获取系列列表增加切片范围校验

--- src/main/java/cn/hanglok/aspect/ParameterEvaluatorAspect.java
+++ src/main/java/cn/hanglok/aspect/ParameterEvaluatorAspect.java
@@ -2,11 +2,8 @@ package cn.hanglok.aspect;
 
 import cn.hanglok.exception.ResponseException;
 import cn.hanglok.util.ConditionEvaluator;
-import jakarta.servlet.http.HttpServletRequest;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
