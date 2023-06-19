d740e7ac9f8813d590e3ceef9e9a3eadf03b01ee
Allen <allen.huang@hanglok-tech.cn>
2023-06-19 17:59:44 +0800

    维度模块控制器

---
+++ src/main/java/cn/hanglok/controller/DimensionController.java
@@ -0,0 +1,11 @@
+package cn.hanglok.controller;
+
+/**
+ * @author Allen
+ * @version 1.0
+ * @className DimensionController
+ * @description TODO
+ * @date 2023/6/19 10:22
+ */
+public class DimensionController {
+}
