e6091c956258ab0bfd0f1ad6f87925d967fe5d03
allen-iMac <allen.huang@hanglok-tech.cn>
2023-07-03 14:36:50 +0800

    弃用Dicom mapper

--- src/main/java/cn/hanglok/mapper/DicomInfoMapper.java
+++ src/main/java/cn/hanglok/mapper/DicomInfoMapper.java
@@ -1,14 +1,15 @@
-package cn.hanglok.mapper;
-
-import cn.hanglok.dto.SimpleSeriesOutDto;
-
-import java.util.List;
-
-/**
- * @author Allen
- */
-public interface DicomInfoMapper {
-    List<SimpleSeriesOutDto> getSimpleDicomInfo(Long institutionId, String modality, String patientSex, String sliceRange, int pageSize, int offset);
-
-    List<String> getModality();
-}
+//package cn.hanglok.mapper;
+//
+//import cn.hanglok.dto.SimpleSeriesOutDto;
+//import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+//
+//import java.util.List;
+//
+///**
+// * @author Allen
+// */
+//public interface DicomInfoMapper {
+//    List<SimpleSeriesOutDto> getSimpleDicomInfo(Page<SimpleSeriesOutDto> page, Long institutionId, String modality, String patientSex, String sliceRange);
+//
+//    List<String> getModality();
+//}
