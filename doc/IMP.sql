556c7edd6cfb6405dcf03ede12cf42642b9ec4e0
allen-MacAir <allen.huang@hanglok-tech.cn>
2023-08-02 15:46:13 +0800

    更新数据库表

--- doc/IMP.sql
+++ doc/IMP.sql
@@ -20,8 +20,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 -- Table structure for body_part
 -- ----------------------------
-DROP TABLE IF EXISTS `body_part`;
-CREATE TABLE `body_part` (
+CREATE TABLE IF NO EXISTS `body_part` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '自增 id',
   `body_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '身体检查部位名称',
   `created_at` timestamp NOT NULL COMMENT '创建时间',
@@ -36,23 +35,21 @@ CREATE TABLE `body_part` (
 -- ----------------------------
 -- Table structure for dictionary
 -- ----------------------------
-DROP TABLE IF EXISTS `dictionary`;
-CREATE TABLE `dictionary`(
-                             `id`         bigint                                                        NOT NULL COMMENT '自增 id',
-                             `name`       varchar(50)                                                   NOT NULL COMMENT '字典名',
-                             `value`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '属性（用英文逗号分隔）',
-                             `created_at` timestamp                                                     NOT NULL COMMENT '创建时间',
-                             `updated_at` timestamp                                                     NOT NULL COMMENT '修改时间',
-                             `creator`    bigint                                                        NOT NULL DEFAULT '-1' COMMENT '创建人',
-                             `updater`    bigint                                                        NOT NULL DEFAULT '-1' COMMENT '修改人',
-                             PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='字典表';
+CREATE TABLE IF NO EXISTS `dictionary`(
+  `id`         bigint                                                        NOT NULL AUTO_INCREMENT COMMENT '自增 id',
+  `name`       varchar(50)                                                   NOT NULL COMMENT '字典名',
+  `value`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '属性（用英文逗号分隔）',
+  `created_at` timestamp                                                     NOT NULL COMMENT '创建时间',
+  `updated_at` timestamp                                                     NOT NULL COMMENT '修改时间',
+  `creator`    bigint                                                        NOT NULL DEFAULT '-1' COMMENT '创建人',
+  `updater`    bigint                                                        NOT NULL DEFAULT '-1' COMMENT '修改人',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='字典表';
 
 -- ----------------------------
 -- Table structure for human_organ
 -- ----------------------------
-DROP TABLE IF EXISTS `human_organ`;
-CREATE TABLE `human_organ` (
+CREATE TABLE IF NO EXISTS `human_organ` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '自增 id',
   `organ_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '器官名称',
   `created_at` timestamp NOT NULL COMMENT '创建时间',
@@ -67,8 +64,7 @@ CREATE TABLE `human_organ` (
 -- ----------------------------
 -- Table structure for label_organ
 -- ----------------------------
-DROP TABLE IF EXISTS `label_organ`;
-CREATE TABLE `label_organ`
+CREATE TABLE IF NO EXISTS `label_organ`
 (
     `id`         bigint    NOT NULL AUTO_INCREMENT COMMENT '自增 id',
     `label_id`   bigint    NOT NULL COMMENT '标签 id',
@@ -79,15 +75,12 @@ CREATE TABLE `label_organ`
     `updater`    bigint    NOT NULL DEFAULT '-1' COMMENT '修改人',
     PRIMARY KEY (`id`),
     UNIQUE KEY `label_id` (`label_id`, `organ_id`) USING BTREE
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci COMMENT ='标签对应器官表';
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT ='标签对应器官表';
 
 -- ----------------------------
 -- Table structure for series_body_part
 -- ----------------------------
-DROP TABLE IF EXISTS `series_body_part`;
-CREATE TABLE `series_body_part`
+CREATE TABLE IF NO EXISTS `series_body_part`
 (
     `id`           bigint    NOT NULL AUTO_INCREMENT COMMENT '自增 id',
     `series_id`    bigint    NOT NULL COMMENT '系列 id',
@@ -98,15 +91,12 @@ CREATE TABLE `series_body_part`
     `updater`      bigint    NOT NULL DEFAULT '-1' COMMENT '修改人',
     PRIMARY KEY (`id`),
     UNIQUE KEY `series_id` (`series_id`, `body_part_id`) USING BTREE
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci COMMENT ='系列对应身体部位表';
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT ='系列对应身体部位表';
 
 -- ----------------------------
 -- Table structure for series_scan_type
 -- ----------------------------
-DROP TABLE IF EXISTS `series_scan_type`;
-CREATE TABLE `series_scan_type`
+CREATE TABLE IF NO EXISTS `series_scan_type`
 (
     `id`           bigint    NOT NULL AUTO_INCREMENT COMMENT '自增 id',
     `series_id`    bigint    NOT NULL COMMENT '系列 id',
@@ -117,20 +107,17 @@ CREATE TABLE `series_scan_type`
     `updater`      bigint    NOT NULL DEFAULT '-1' COMMENT '修改人',
     PRIMARY KEY (`id`),
     UNIQUE KEY `series_id` (`series_id`, `scan_type_id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci COMMENT ='系列对应扫描类型表';
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT ='系列对应扫描类型表';
 
 -- ----------------------------
 -- Table structure for image_instances
 -- ----------------------------
-DROP TABLE IF EXISTS `image_instances`;
-CREATE TABLE `image_instances`
+CREATE TABLE IF NO EXISTS `image_instances`
 (
-    `id`              bigint                                                        NOT NULL AUTO_INCREMENT COMMENT '自增 id',
-    `instance_number` int                                                           NOT NULL COMMENT '实例号',
-    `instance_uid`    varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '实例 uid',
-    `slice_location`  double                                                        NOT NULL COMMENT '切片位置',
+  `id`              bigint                                                        NOT NULL AUTO_INCREMENT COMMENT '自增 id',
+  `instance_number` int                                                           NOT NULL COMMENT '实例号',
+  `instance_uid`    varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '实例 uid',
+  `slice_location`  double                                                        NOT NULL COMMENT '切片位置',
   `instance_at` timestamp NULL DEFAULT NULL COMMENT '实例创建时间',
   `series_id` bigint NOT NULL COMMENT '系列 id',
   `created_at` timestamp NOT NULL COMMENT '创建时间',
@@ -139,13 +126,12 @@ CREATE TABLE `image_instances`
   `updater` bigint NOT NULL DEFAULT '-1' COMMENT '修改人',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `id` (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=37249 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='实例表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='实例表';
 
 -- ----------------------------
 -- Table structure for image_label
 -- ----------------------------
-DROP TABLE IF EXISTS `image_label`;
-CREATE TABLE `image_label` (
+CREATE TABLE IF NO EXISTS `image_label` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '自增 id',
   `file_name` varchar(50) NOT NULL COMMENT '文件名',
   `file_location` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '文件路径',
@@ -161,8 +147,7 @@ CREATE TABLE `image_label` (
 -- ----------------------------
 -- Table structure for image_scan_type
 -- ----------------------------
-DROP TABLE IF EXISTS `image_scan_type`;
-CREATE TABLE `image_scan_type` (
+CREATE TABLE IF NO EXISTS `image_scan_type` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '自增 id',
   `scan_type_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '扫描类型名称',
   `created_at` timestamp NOT NULL COMMENT '创建时间',
@@ -177,8 +162,7 @@ CREATE TABLE `image_scan_type` (
 -- ----------------------------
 -- Table structure for image_series
 -- ----------------------------
-DROP TABLE IF EXISTS `image_series`;
-CREATE TABLE `image_series` (
+CREATE TABLE IF NO EXISTS `image_series` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '自增 id',
   `series_number` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '系列号（单个设备唯一）',
   `series_uid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '系列uid',
@@ -199,13 +183,12 @@ CREATE TABLE `image_series` (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `id` (`id`) USING BTREE,
   UNIQUE KEY `series_uid` (`series_uid`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=342 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='系列表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='系列表';
 
 -- ----------------------------
 -- Table structure for image_studies
 -- ----------------------------
-DROP TABLE IF EXISTS `image_studies`;
-CREATE TABLE `image_studies` (
+CREATE TABLE IF NO EXISTS `image_studies` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '自增 id',
   `study_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '研究 id',
   `study_uid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '研究 uid',
@@ -219,13 +202,12 @@ CREATE TABLE `image_studies` (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `id` (`id`) USING BTREE,
   UNIQUE KEY `study_uid` (`study_uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=86 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='研究表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='研究表';
 
 -- ----------------------------
 -- Table structure for institution
 -- ----------------------------
-DROP TABLE IF EXISTS `institution`;
-CREATE TABLE `institution` (
+CREATE TABLE IF NO EXISTS `institution` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '自增 id',
   `institution_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '机构名称',
   `institution_address` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '机构地址',
@@ -236,13 +218,12 @@ CREATE TABLE `institution` (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `institution_tb_UN_id` (`id`) USING BTREE,
   UNIQUE KEY `institution_tb_UN_name` (`institution_name`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='机构表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='机构表';
 
 -- ----------------------------
 -- Table structure for institution_patient
 -- ----------------------------
-DROP TABLE IF EXISTS `institution_patient`;
-CREATE TABLE `institution_patient` (
+CREATE TABLE IF NO EXISTS `institution_patient` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '自增 id',
   `patient_number` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '患者 id',
   `patient_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '患者姓名',
@@ -254,6 +235,6 @@ CREATE TABLE `institution_patient` (
   `updater` bigint NOT NULL DEFAULT '-1' COMMENT '修改人',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='患者表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='患者表';
 
 SET FOREIGN_KEY_CHECKS = 1;
