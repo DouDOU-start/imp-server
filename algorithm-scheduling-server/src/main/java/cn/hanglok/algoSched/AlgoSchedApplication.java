330d763131b0e1c0e0596c1229cbfa597255b4dc
allen-MacAir <allen.huang@hanglok-tech.cn>
2023-09-18 15:35:26 +0800

    新增算法调度服务

---
+++ algorithm-scheduling-server/src/main/java/cn/hanglok/algoSched/AlgoSchedApplication.java
@@ -0,0 +1,18 @@
+package cn.hanglok.algoSched;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Allen
+ * @version 1.0
+ * @className AlgoSchedApplication
+ * @description 算法调度服务启动程序
+ * @date 2023/9/18
+ */
+@SpringBootApplication
+public class AlgoSchedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoSchedApplication.class, args);
+    }
+}
