286e29a1698726141278926b14a565ff354f087a
allen-MacAir <allen.huang@hanglok-tech.cn>
2024-01-24 23:35:50 +0800

    容器自重启

--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,4 @@
 
 # docker run -itd --name pacs --net=host hanglok/pacs:v1.0.0
 
-docker run -itd --name algorithm-scheduling --net=host hanglok/algorithm_scheduling:v1.0.0
+docker run -itd --name algorithm-scheduling --restart=always --net=host hanglok/algorithm_scheduling:v1.0.0
